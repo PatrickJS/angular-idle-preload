fe18a196952b5010b4dd15c87457b6eb2e1ff562
PatrickJS <github@gdi2290.com>
2016-11-29 11:50:20 -0500

    refactor: use REQUEST_IDLE_CALLBACK

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,35 @@
-import { NgZone, NgModule, ModuleWithProviders } from '@angular/core';
-import { RouterModule, PreloadingStrategy, Route } from '@angular/router';
+import { NgZone, NgModule, ModuleWithProviders, Inject, OpaqueToken } from '@angular/core';
+// import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
  /*
+  * token to requestIdleCallback
+  */
+export const REQUEST_IDLE_CALLBACK = new OpaqueToken('REQUEST_IDLE_CALLBACK');
+
+ /*
   * Private please use @angularclass/request-idle-callback if you want to use this
   */
-function requestIdle(zone: NgZone, fn: () => any) {
+export function _requestIdle(zone: NgZone) {
   let win: any = window;
   if (win.requestIdleCallback) {
-    return win.requestIdleCallback(fn);
+    return win.requestIdleCallback;
   }
-  return zone.runOutsideAngular(() => win.setTimeout(fn, 10));
+  return (fn) => zone.runOutsideAngular(() => win.setTimeout(fn, 10));
 }
 
-export class IdlePreload implements PreloadingStrategy {
+export class IdlePreload /*implements PreloadingStrategy*/ {
  /*
   * include zone to run outside of zone.js
   */
-  constructor(private _ngZone: NgZone) {}
+  constructor(private _ngZone: NgZone, @Inject(REQUEST_IDLE_CALLBACK) private requestIdleCallback: any) {}
 
  /*
   * fire off preloading async modules
   */
-  preload(route: Route, fn: () => Observable<any>): Observable<any> {
-    requestIdle(this._ngZone, fn);
+  preload(route: /*Route*/ any, fn: () => Observable<any>): Observable<any> {
+    this.requestIdleCallback(fn);
     return Observable.of(null);
   }
 
@@ -37,6 +42,10 @@ export const ANGULARCLASS_IDLE_PRELOAD_PROVIDERS: any[] = [
   { provider: IdlePreload, useClass: IdlePreload }
 ];
 
+export const ANGULARCLASS_REQUEST_IDLE_CALLBACK_PROVIDERS: any[] = [
+  { provider: REQUEST_IDLE_CALLBACK, useFactory: _requestIdle, deps: [ NgZone ] }
+];
+
 @NgModule({
   // because Angular
 })
@@ -44,8 +53,13 @@ export class IdlePreloadModule {
  /*
   * forRoot() to allow providers only be created once
   */
-  static forRoot(): ModuleWithProviders {
-    return { ngModule: IdlePreloadModule, providers: ANGULARCLASS_IDLE_PRELOAD_PROVIDERS }
+  static forRoot(config: any = {}): ModuleWithProviders {
+    return {
+      ngModule: IdlePreloadModule,
+      providers: [
+        ...(config.requestIdleCallback === false ? [] : ANGULARCLASS_REQUEST_IDLE_CALLBACK_PROVIDERS),
+        ...ANGULARCLASS_IDLE_PRELOAD_PROVIDERS
+      }
   }
 
  /*
